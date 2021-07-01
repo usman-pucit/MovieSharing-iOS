9f7f4ffd8fc392089f253013f096a983c6c8ddb7
Muhammad Usman <usmans1020@gmail.com>
2021-07-01 07:19:03 +0500

    removed App coordinator

--- MovieSharing-iOS/Application/SceneDelegate.swift
+++ MovieSharing-iOS/Application/SceneDelegate.swift
@@ -10,19 +10,8 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var coordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let window = window else { return }
-
-        let navController = UINavigationController()
-
-        // Setting up application navigation
-        coordinator = AppCoordinator(navigationController: navController)
-        coordinator?.start()
-
-        window.rootViewController = navController
-        window.makeKeyAndVisible()
     }
 }
 
